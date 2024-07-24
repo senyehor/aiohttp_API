b35fb1fc391cebd02080cc92d3839afc1c9b5467
Yehor Sencha <senyehor@gmail.com>
2024-07-24 15:10:38 +0300

    feat: add user factory for tests

---
+++ repository/tests/factories.py
@@ -0,0 +1,15 @@
+import factory
+from faker import Faker
+
+from repository.schemas import APIUser
+
+fake = Faker()
+
+
+class UserFactory(factory.Factory):
+    class Meta:
+        model = APIUser
+
+    id = factory.Sequence(lambda n: n + 1)
+    email = factory.LazyAttribute(lambda _: fake.email())
+    password = factory.LazyAttribute(lambda _: fake.password())
