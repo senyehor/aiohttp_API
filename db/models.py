a82af769d9ae9d7c6f6f371766c4a33a14bfee07
Yehor Sencha <senyehor@gmail.com>
2024-07-25 11:03:50 +0300

    feat: create db models

---
+++ db/models.py
@@ -0,0 +1,26 @@
+import peewee
+from peewee import CharField, ForeignKeyField
+
+from db.connection import database
+
+
+class BaseModel(peewee.Model):
+    class Meta:
+        database = database
+
+
+class APIUser(BaseModel):
+    email = CharField(unique=True, index=True, null=False)
+    password = CharField(null=False)
+
+
+class Location(BaseModel):
+    name = CharField(null=False)
+
+
+class Device(BaseModel):
+    type = CharField(null=False)
+    login = CharField(null=False)
+    password = CharField(null=False)
+    location = ForeignKeyField(Location, backref='devices', null=False, on_delete='RESTRICT')
+    owner = ForeignKeyField(APIUser, backref='devices', null=False, on_delete='RESTRICT')
