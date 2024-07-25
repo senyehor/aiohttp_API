6b8190473154a3dbc0f5231fdbac89726fa37fbb
Yehor Sencha <senyehor@gmail.com>
2024-07-25 13:45:42 +0300

    ref: create function to make a database

--- db/connection.py
+++ db/connection.py
@@ -1,9 +1,17 @@
+from peewee_async import AsyncDatabase
 from peewee_asyncext import PostgresqlExtDatabase
 
-from config import DB_SETTINGS
+from config import DB_SETTINGS, _DBSettings
 
-_ = DB_SETTINGS
-database = PostgresqlExtDatabase(
-    _.db_name, user=_.db_user, password=_.db_password,
-    host=_.db_host, port=_.db_port
-)
+
+def create_database_from_settings(
+        settings: _DBSettings, db_creator: type[AsyncDatabase]
+) -> AsyncDatabase:
+    _ = settings
+    return db_creator(
+        _.db_name, user=_.db_user, password=_.db_password,
+        host=_.db_host, port=_.db_port
+    )
+
+
+database = create_database_from_settings(DB_SETTINGS, PostgresqlExtDatabase)
