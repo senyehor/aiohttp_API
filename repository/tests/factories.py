e9a43dd9ecd3e288097df7e3f632672f99fe9ee8
Yehor Sencha <senyehor@gmail.com>
2024-07-26 18:37:59 +0300

    ref: switch to direct imports

--- repository/tests/factories.py
+++ repository/tests/factories.py
@@ -1,4 +1,4 @@
-import factory
+from factory import Factory, LazyAttribute, Sequence
 from faker import Faker
 
 from repository.schemas import UserSchema
@@ -6,10 +6,10 @@ from repository.schemas import UserSchema
 fake = Faker()
 
 
-class UserFactory(factory.Factory):
+class UserFactory(Factory):
     class Meta:
         model = UserSchema
 
-    id = factory.Sequence(lambda n: n + 1)
-    email = factory.LazyAttribute(lambda _: fake.email())
-    password = factory.LazyAttribute(lambda _: fake.password())
+    id = Sequence(lambda n: n + 1)
+    email = LazyAttribute(lambda _: fake.email())
+    password = LazyAttribute(lambda _: fake.password())
