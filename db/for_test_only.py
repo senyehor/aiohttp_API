a92d1b4ce64fad5c1c207668cae5074bdcf88d93
Yehor Sencha <senyehor@gmail.com>
2024-07-26 21:07:00 +0300

    fix: make models ordered to drop correctly

--- db/for_test_only.py
+++ db/for_test_only.py
@@ -10,11 +10,11 @@ from db.models import Device, Location, User
 
 class _DBTestOnlySettings(DBSettings):
     class Config:
-        env_file = f'{Path(__file__).resolve().parent}/test.env'
+        env_file = f'{Path(__file__).resolve().parent.parent}/test.env'
         env_file_encoding = 'utf-8'
 
 
-_MODELS = [User, Device, Location]
+_MODELS_ORDERED_TO_DROP_CORRECTLY = (Device, User, Location)
 # this typehint allows to have correct suggestions, even though
 # it is not supported to be used with a sync db
 _TEST_DB: Database | AsyncDatabase = create_database_from_settings(
@@ -26,21 +26,21 @@ _TEST_DB: Database | AsyncDatabase = create_database_from_settings(
 def _setup_test_db():
     """make models use the test db, create tables"""
     _TEST_DB.bind(
-        _MODELS,
+        _MODELS_ORDERED_TO_DROP_CORRECTLY,
         bind_refs=False, bind_backrefs=False
     )
     _TEST_DB.connect()
-    _TEST_DB.create_tables(_MODELS)
+    _TEST_DB.create_tables(_MODELS_ORDERED_TO_DROP_CORRECTLY)
 
 
 def _teardown_test_db():
     """drop tables and close db"""
-    _TEST_DB.drop_tables(_MODELS)
+    _TEST_DB.drop_tables(_MODELS_ORDERED_TO_DROP_CORRECTLY)
     _TEST_DB.close()
 
 
 async def _flush_db():
-    for model in _MODELS:
+    for model in _MODELS_ORDERED_TO_DROP_CORRECTLY:
         await execute(
             model
             .delete()
