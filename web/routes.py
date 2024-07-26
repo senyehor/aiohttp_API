fe333c0ab2451e709b9c7b9d8544db44466708eb
Yehor Sencha <senyehor@gmail.com>
2024-07-26 17:59:58 +0300

    fix: move router urls adding to router file itself, as it was not evaluated when added inside views file

--- web/routes.py
+++ web/routes.py
@@ -1,3 +1,7 @@
 from aiohttp.web_routedef import RouteTableDef
 
+from web.views import DeviceCRUDView
+
 router = RouteTableDef()
+
+router.view('/devices')(DeviceCRUDView)
--- web/views.py
+++ web/views.py
@@ -1,8 +1,6 @@
 from logic.services import DeviceService
 from web.crud_base_view import CRUDViewBase
-from web.routes import router
 
 
-@router.view('/devices')
 class DeviceCRUDView(CRUDViewBase):
     service = DeviceService()
