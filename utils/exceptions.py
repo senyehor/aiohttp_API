f2d7aaa2c05903cb1862c0687c74673fa1ed531b
Yehor Sencha <senyehor@gmail.com>
2024-07-26 14:17:54 +0300

    feat: add exception with message for user

--- utils/exceptions.py
+++ utils/exceptions.py
@@ -1,2 +1,6 @@
 class AppExceptionBase(Exception):
     """base class for all app exception"""
+
+
+class AppExceptionWithMessageForUser(AppExceptionBase):
+    """also includes .message to provide meaningful feedback to user"""
