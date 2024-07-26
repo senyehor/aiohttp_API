f25acdb1ea3dd57d1a2f03b7ba9a196b8c00e8e4
Yehor Sencha <senyehor@gmail.com>
2024-07-26 17:40:57 +0300

    fix: add model serialization

--- logic/services.py
+++ logic/services.py
@@ -1,35 +1,57 @@
-from typing import Iterable
+from typing import Any, Callable, Iterable, TypeAlias
+
+from playhouse.shortcuts import model_to_dict
 
 from logic.config import DEFAULT_PAGE_SIZE, FIRST_PAGE
-from repository.repositories.bases import CRUDRepositoryBase
+from repository.repositories.bases import CRUDRepositoryBase, ModelObject
 from repository.repositories.implementations import (
     DevicePeeWeeRepository,
     LocationPeeWeeRepository, UserPeeWeeRepository,
 )
-from utils.crud_protocol import CreatedObjectId, CRUDOperationsBase, ModelObject
+from utils.crud_protocol import CreatedObjectId, CRUDOperationsBase, OperationSucceeded
+
+SerializedModel: TypeAlias = dict[str, Any]
 
 
 class CRUDServiceBase(CRUDOperationsBase):
     repository: CRUDRepositoryBase
+    model_to_dict_serializer: Callable[[ModelObject, ...], SerializedModel] = model_to_dict
 
-    def __init__(self, repository: CRUDRepositoryBase = None) -> None:
+    def __init__(
+            self, repository: CRUDRepositoryBase = None,
+            model_to_dict_converter: Callable[[ModelObject], SerializedModel] = None
+    ) -> None:
         # use the attribute of the instance class
         # to allow convenient subclassing, with providing default repository
         self.repository = repository or self.__class__.repository
         if not self.repository:
             raise ValueError('repository must be either passed or set at a child class')
+        self.model_to_dict_serializer = \
+            model_to_dict_converter or self.__class__.model_to_dict_serializer
+        if not self.repository:
+            raise ValueError(
+                'model_to_dict_serializer must be either passed or set at a child class'
+            )
 
-    async def get_object_by_id(self, object_id: int) -> ModelObject:
-        return await self.repository.get_object_by_id(object_id)
+    async def get_object_by_id(self, object_id: int, **serializer_kwargs) -> SerializedModel:
+        retrieved_object = await self.repository.get_object_by_id(object_id)
+        return self.model_to_dict_serializer(
+            retrieved_object,
+            **serializer_kwargs
+        )
 
     async def add_object(self, **fields) -> CreatedObjectId:
         return await self.repository.add_object(**fields)
 
     async def get_objects(
             self, page_number: int = FIRST_PAGE,
-            page_size: int = DEFAULT_PAGE_SIZE
-    ) -> Iterable[ModelObject]:
-        return await self.repository.get_objects(page_number, page_size)
+            page_size: int = DEFAULT_PAGE_SIZE,
+            **serializer_kwargs
+    ) -> Iterable[SerializedModel]:
+        return [
+            self.model_to_dict_serializer(model, **serializer_kwargs) for model in
+            await self.repository.get_objects(page_number, page_size)
+        ]
 
     async def update_object(self, object_id: int, **fields) -> OperationSucceeded:
         return await self.repository.update_object(object_id, **fields)
