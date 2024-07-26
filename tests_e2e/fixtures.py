74def354598310066678cb1c429cca23dc492319
Yehor Sencha <senyehor@gmail.com>
2024-07-26 18:52:10 +0300

    feat: create fixtures for user, location and device

--- tests_e2e/fixtures.py
+++ tests_e2e/fixtures.py
@@ -2,6 +2,7 @@ import pytest
 import pytest_asyncio
 
 from db.for_test_only import _flush_db, _setup_test_db, _teardown_test_db
+from repository.tests.factories import DeviceFactory, LocationFactory, UserFactory
 
 
 @pytest.fixture(scope='module')
@@ -15,3 +16,18 @@ def create_test_db():
 async def flush_db():
     yield
     await _flush_db()
+
+
+@pytest.fixture()
+def user():
+    return UserFactory()
+
+
+@pytest.fixture()
+def location():
+    return LocationFactory()
+
+
+@pytest.fixture()
+def device(user, location):
+    return DeviceFactory(owner=user, location=location)
