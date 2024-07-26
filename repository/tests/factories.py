bc2a4c9dfc9f4f9bd98a4a2b38edb580624bcf8d
Yehor Sencha <senyehor@gmail.com>
2024-07-26 18:46:59 +0300

    feat: create factories for location and device

--- repository/tests/factories.py
+++ repository/tests/factories.py
@@ -1,7 +1,9 @@
-from factory import Factory, LazyAttribute, Sequence
+from random import choice
+
+from factory import Factory, lazy_attribute, LazyAttribute, Sequence, SubFactory
 from faker import Faker
 
-from repository.schemas import UserSchema
+from repository.schemas import DeviceSchema, LocationSchema, UserSchema
 
 fake = Faker()
 
@@ -13,3 +15,32 @@ class UserFactory(Factory):
     id = Sequence(lambda n: n + 1)
     email = LazyAttribute(lambda _: fake.email())
     password = LazyAttribute(lambda _: fake.password())
+
+
+class LocationFactory(Factory):
+    class Meta:
+        model = LocationSchema
+
+    id = Sequence(lambda n: n + 1)
+    name = LazyAttribute(lambda _: fake.address())
+
+
+class DeviceFactory(Factory):
+    class Meta:
+        model = DeviceSchema
+
+    type = LazyAttribute(lambda _: choice(['smart_watch', 'smart_fridge', 'smart_lamp']))
+    login = LazyAttribute(lambda _: fake.word())
+    password = LazyAttribute(lambda _: fake.password())
+    location: LocationFactory = SubFactory(LocationFactory)
+    owner: UserFactory = SubFactory(UserFactory)
+
+    @lazy_attribute
+    def location_id(self):
+        # pylint: disable=no-member
+        return self.location.id
+
+    @lazy_attribute
+    def owner_id(self):
+        # pylint: disable=no-member
+        return self.owner.id
